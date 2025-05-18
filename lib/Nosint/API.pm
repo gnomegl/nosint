8f4eeee254ce1f922bb69d030190b145340c8b36
gnomegl <gnome@groomla.ke>
2025-05-18 02:48:41 -0400

    add api client for nosint.org with three core modules

---
+++ lib/Nosint/API.pm
@@ -0,0 +1,93 @@
+package Nosint::API;
+
+use strict;
+use warnings;
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request;
+use Time::HiRes qw(time);
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $self = {
+        cookie => $args{cookie},
+        verbose => $args{verbose} || 0,
+        formatter => $args{formatter},
+        buffer => '',
+        ua => LWP::UserAgent->new,
+    };
+
+    $self->{ua}->timeout(180);
+    $self->{ua}->agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
+    bless $self, $class;
+    return $self;
+}
+
+sub search {
+    my ($self, $target, $plugin_type) = @_;
+
+    my $url = "https://nosint.org/api/stream-search?target=$target&plugin_type=$plugin_type";
+
+    my $req = HTTP::Request->new(GET => $url);
+    $req->header('Accept' => 'text/event-stream');
+    $req->header('Accept-Language' => 'en-US,en;q=0.5');
+    $req->header('Accept-Encoding' => 'gzip, deflate, br, zstd');
+    $req->header('Cookie' => $self->{cookie});
+    $req->header('Connection' => 'keep-alive');
+    $req->header('Cache-Control' => 'no-cache');
+
+    $self->{formatter}->print_info("Starting search for $target with plugin type: $plugin_type");
+
+    my $response = $self->{ua}->request($req, sub {
+        my ($data, $response, $protocol) = @_;
+
+        $self->{buffer} .= $data;
+
+        while ($self->{buffer} =~ s/^data: (.+)(\r?\n)+//m) {
+            my $json_str = $1;
+            $self->process_data($json_str) if $json_str;
+        }
+    });
+
+    if (!$response->is_success) {
+        $self->{formatter}->print_error(
+            "Error connecting to API: " . $response->status_line . "\n" .
+            "Response body: " . $response->content . "\n\n" .
+            "This could be due to cookie expiration or incorrect format.\n" .
+            "Please check your cookie is correct and current."
+        );
+        return 0;
+    }
+
+    return 1;
+}
+
+sub process_data {
+    my ($self, $json_str) = @_;
+
+    # skip empty lines
+    return if $json_str =~ /^\s*$/;
+
+    my $data;
+    eval {
+        $data = decode_json($json_str);
+    };
+
+    if ($@) {
+        $self->{formatter}->print_error("Error parsing JSON: $@\nRaw data: $json_str");
+        return;
+    }
+
+    $self->{formatter}->format_output($data, $json_str);
+}
+
+sub validate_auth {
+    my ($self) = @_;
+    return 0 unless $self->{cookie};
+    my $valid = $self->{cookie} =~ /next-auth\.session-token=/;
+    return $valid;
+}
+
+1;
---
+++ lib/Nosint/CLI.pm
@@ -0,0 +1,117 @@
+package Nosint::CLI;
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Nosint::API;
+use Nosint::Formatter;
+
+sub new {
+    my ($class) = @_;
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
+
+sub run {
+    my ($self) = @_;
+
+    my $options = $self->parse_options();
+
+    return 0 if $options->{help};
+
+    if (!$options->{target} || !$options->{plugin_type}) {
+        $self->print_help();
+        return 1;
+    }
+
+    my $formatter = Nosint::Formatter->new(
+        json_output => $options->{json_output},
+        show_not_found => $options->{show_not_found},
+    );
+
+    $options->{cookie} = $ENV{NOSINT_COOKIE} unless $options->{cookie};
+
+    if (!$options->{cookie}) {
+        $formatter->print_error("Cookie not provided via --cookie flag or NOSINT_COOKIE environment variable");
+        print "Please provide your authentication cookie from nosint.org\n";
+        return 1;
+    }
+
+    my $api = Nosint::API->new(
+        cookie => $options->{cookie},
+        verbose => $options->{verbose},
+        formatter => $formatter,
+    );
+
+    if (!$api->validate_auth()) {
+        $formatter->print_error("Invalid authentication cookie format");
+        print "Please check your cookie format and try again\n";
+        return 1;
+    }
+
+    my $result = $api->search($options->{target}, $options->{plugin_type});
+
+    return $result ? 0 : 1;
+}
+
+sub parse_options {
+    my ($self) = @_;
+
+    my %options = (
+        json_output => 0,
+        help => 0,
+        verbose => 0,
+        show_not_found => 0,
+    );
+
+    GetOptions(
+        "json|j" => \$options{json_output},
+        "help|h" => \$options{help},
+        "target|t=s" => \$options{target},
+        "plugin-type|p=s" => \$options{plugin_type},
+        "cookie|c=s" => \$options{cookie},
+        "verbose|v" => \$options{verbose},
+        "show-not-found" => \$options{show_not_found},
+    );
+
+    if ($options{help}) {
+        $self->print_help();
+    }
+
+    return \%options;
+}
+
+sub print_help {
+    my ($self) = @_;
+
+    print <<EOF;
+Usage: nosint.pl --target EMAIL --plugin-type TYPE [options]
+
+Required arguments:
+  --target, -t EMAIL          Target email address to search
+  --plugin-type, -p TYPE      Plugin type (e.g., 'email')
+
+Options:
+  --cookie, -c COOKIE         Authentication cookie for nosint.org
+  --json, -j                  Output in JSONL format
+  --help, -h                  Show this help message
+  --show-not-found            Show not found results (default: off)
+  --verbose, -v               Show verbose output
+
+Environment variables:
+  NOSINT_COOKIE               Authentication cookie (if not provided with --cookie)
+
+Example:
+  Set the authentication cookie in environment
+  export NOSINT_COOKIE='next-auth.csrf-token=value; next-auth.callback-url=value; next-auth.session-token=value'
+
+  Run the search
+  nosint.pl --target user\@example.com --plugin-type email
+
+  Alternative: Provide cookie directly
+  nosint.pl --target user\@example.com --plugin-type email --cookie 'next-auth.csrf-token=value; next-auth.callback-url=value; next-auth.session-token=value'
+EOF
+}
+
+1;
